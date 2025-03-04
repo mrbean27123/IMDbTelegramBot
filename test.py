6fa79229b0cb635f18042aa3a7d89c07ead2c347
mrbean27123 <mrbean27123@gmail.com>
2025-03-04 15:40:17 +0200

    Refractoring for SOLID

--- test.py
+++
@@ -1,9 +0,0 @@
-import datetime
-
-from database import db
-from movie import Movie
-
-not_released_movies = db.get_table(table_name="Фильмы", data=Movie(date_now=None).to_dict)
-for movie in not_released_movies:
-    print(Movie.from_dict(movie))
-
